fd366eec0bd6a71d6b3e1db4b244a79d4c36f4b1
zack <123098284+WillJe65@users.noreply.github.com>
2025-11-12 07:35:35 +0700

    add so it can be deploy for testing

--- BACKEND/db.js
+++ BACKEND/db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2')
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: process.env.DB
+  host:   process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 db.connect(err => {
